ff03686150e2310060a661c37b3eb0f5d1c0904e
Toby Clemson <tobyclemson@gmail.com>
2021-02-22 20:57:38 +0000

    Remove unused Mongo DB Atlas client methods.

--- spec/support/mongo_db_atlas_client.rb
+++ spec/support/mongo_db_atlas_client.rb
@@ -12,32 +12,9 @@ class MongoDBAtlasClient
         ENV["MONGODB_ATLAS_PRIVATE_KEY"]
   end
 
-  def get_one_project(project_id)
-    JSON.parse(
-        get("/groups/#{project_id}").body)
-  end
-
-  def get_project_ip_access_list(project_id)
-    JSON.parse(get("/groups/#{project_id}/accessList").body)
-  end
-
   def get_one_database_user(project_id, username)
     JSON.parse(
         get("/groups/#{project_id}/databaseUsers/admin/#{username}").body)
-  end
-
-  def get_all_teams_assigned_to_project(project_id)
-    JSON.parse(get("/groups/#{project_id}/teams").body)
-  end
-
-  def get_one_team_by_name(organisation_id, team_name)
-    JSON.parse(
-        get("/orgs/#{organisation_id}/teams/byName/#{team_name}").body)
-  end
-
-  def get_all_users_assigned_to_team(organisation_id, team_id)
-    JSON.parse(
-        get("/orgs/#{organisation_id}/teams/#{team_id}/users").body)
   end
 
   def get_one_cluster(project_id, cluster_name)
